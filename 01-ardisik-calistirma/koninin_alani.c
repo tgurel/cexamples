6e89b714f90cbabe7a6627669c00da55d1102b13
refice <48351889+refice@users.noreply.github.com>
2019-03-08 14:08:28 +0300

    Create koninin_alani.c

---
+++ 01-ardisik-calistirma/koninin_alani.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+  double r,h,hacim,alan,s,pi;
+	pi=3.14159;
+    
+	printf("Yaricapi giriniz :");
+  scanf("%lf",&r);
+  printf("Yukseklik giriniz :");
+  scanf("%lf",&h);    
+    
+	hacim=(pi*r*r*h)/3.0;
+  printf("Hacim = %f",hacim);
+
+  s=sqrt(r*r + h*h);
+  alan = pi*r*s + pi*r*r;
+  printf("\nAlan = %f",alan);
+  return 0;
+}
